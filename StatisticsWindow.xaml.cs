37cd07da52539b8d18aab456096fea394f9fc795
Максим Ялунин <maximya@bk.ru>
2025-10-20 10:11:34 +0300

    Реализована логика получения статистики продуктов

--- StatisticsWindow.xaml.cs
+++ StatisticsWindow.xaml.cs
@@ -1,27 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Data.Entity;
 
 namespace ShopApp_4ISIP322
 {
-    /// <summary>
-    /// Логика взаимодействия для StatisticsWindow.xaml
-    /// </summary>
     public partial class StatisticsWindow : Window
     {
         public StatisticsWindow()
         {
             InitializeComponent();
+            LoadStatistics();
+        }
+
+        private void LoadStatistics()
+        {
+            // Получаем единственный экземпляр контекста
+            var context = Entities2.GetContext();
+
+            // Загружаем продукты вместе с категориями
+            var products = context.Products.Include(p => p.Category1).ToList();
+
+            // Общая статистика
+            int totalProducts = products.Sum(p => p.Quantity);
+            decimal totalValue = products.Sum(p => p.Price * p.Quantity);
+            txtTotalProducts.Text = $"Общее количество товаров: {totalProducts}";
+            txtTotalValue.Text = $"Общая стоимость: {totalValue:C}";
+
+            // Статистика по категориям
+            var categoryStats = products
+                .GroupBy(p => p.Category1.CategoryName)
+                .Select(g => new
+                {
+                    Category = g.Key,
+                    AveragePrice = g.Average(p => p.Price)
+                })
+                .ToList();
+            dgCategoryStats.ItemsSource = categoryStats;
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            LoadStatistics();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
